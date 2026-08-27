1e987b4034cff68b1873e025c1c7a67f4f5c77ba
devilsen1999 <devilsen1999@gmail.com>
2026-08-27 15:29:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = "https://ihzsalexazemlqbrpneb.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = sb_publishable_SK209MnGZcXfawWYr1Zm2Q_vDfJSMgj
+const SUPABASE_PUBLISHABLE_KEY =" sb_publishable_SK209MnGZcXfawWYr1Zm2Q_vDfJSMgj"
